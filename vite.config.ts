bba5eb8fd4b1d9f9a12b9f0b715b41fb711a39e1
yashaschandru583-glitch <yashaschandru583@gmail.com>
2026-08-21 11:22:18 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/taskflow/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
